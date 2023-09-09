bb21793f8ca9b71728b40581fa987fbcb142d3c1
emanuel oviedo <emanueloviedo9@gmail.com>
2023-09-09 13:30:46 -0300

    Agregar archivos de proyecto.

---
+++ LP3_2_HugoEmanuelOviedo/WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LP3_2_HugoEmanuelOviedo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Nombre:"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Apellido:"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label6" runat="server" Text="Descripcion:"></asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Telefono:"></asp:Label>
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Enviar" />
+            <br />
+            <asp:Label ID="Label7" runat="server" Text="0"></asp:Label>
+        </div>
+        <p>
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </p>
+    </form>
+</body>
+</html>
---
+++ LP3_2_HugoEmanuelOviedo/WebForm1.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LP3_2_HugoEmanuelOviedo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        List<string> Contacto = new List<string>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            actualizarLista();
+        }
+        // creo una bandera para saber si la pagina se cargo por primera vez
+        int bandera = 0;
+        public void actualizarLista()
+        {
+            if(File.Exists(Server.MapPath(".") + "/datos.txt")) // saber si existe datos.txt
+            {
+                StreamReader archivoRead = new StreamReader(this.Server.MapPath(".") + "/datos.txt"); // enstancia del streamreader
+                string linea; // variable que se agrega a la lista
+                Contacto.Clear(); // limpio la lista por si el metodo se vuelve a llamar que no se sumen al grid y quede el doble de los datos
+                while ((linea = archivoRead.ReadLine()) != null)
+                {
+                    Contacto.Add(linea);
+                }
+                archivoRead.Close();
+                // --- Asigno la lista al grid
+                GridView1.DataSource = Contacto;
+                //--- aca la enlazo
+                GridView1.DataBind();
+            }
+
+        } 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            if(!string.IsNullOrEmpty(TextBox1.Text) && !string.IsNullOrEmpty(TextBox2.Text) && !string.IsNullOrEmpty(TextBox3.Text) && !string.IsNullOrEmpty(TextBox4.Text))
+            {
+                StreamWriter archivo = new StreamWriter(this.Server.MapPath(".") + "/datos.txt", true);
+                archivo.WriteLine("Nombre: " + TextBox1.Text + " Apellido:" + TextBox2.Text + " Descripcion:" + TextBox3.Text + " Telefono:" + TextBox4.Text);
+                archivo.Close();
+                Label7.Text = "Se registro correctamente";
+                actualizarLista();
+            } else
+            {
+                Label7.Text = "Todos los campos tienen que estar completos";
+            }
+            
+            //----
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
